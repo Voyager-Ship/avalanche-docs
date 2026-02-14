44a0f7ca94d924168725d695ae243de5236e0b29
Simón Arboleda Ramirez <simon0820s@gmail.com>
2026-02-14 17:53:11 -0500

    feat(notifications): update consumptions

--- app/api/notifications/get/route.ts
+++ app/api/notifications/get/route.ts
@@ -6,16 +6,18 @@ type GetNotificationsBody = {
 };
 export const runtime: "nodejs" = "nodejs";
 
-const baseUrl: string | undefined = process.env.AVALANCHE_METRICS_URL;
+const baseUrl: string | undefined = process.env.AVALANCHE_WORKERS_URL;
+const avalancheWokersApiKey: string | undefined =
+  process.env.AVALANCHE_WORKERS_API_KEY;
 
 export async function POST(req: any): Promise<Response> {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET ?? '' })
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET ?? "",
+  });
   if (!token) return new Response("Unauthorized", { status: 401 });
-  const encodedToken = await encode({token: token, secret: process.env.NEXTAUTH_SECRET ?? ''})
-  if (!encodedToken) return new Response("Error at get notifications", { status: 500});
-
   try {
-    if (!baseUrl) {
+    if (!baseUrl || !avalancheWokersApiKey) {
       return NextResponse.json({ error: "Failed" }, { status: 500 });
     }
     const upstream: Response = await fetch(
@@ -24,17 +26,18 @@ export async function POST(req: any): Promise<Response> {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "authorization": encodedToken
+          "x-api-key": avalancheWokersApiKey,
         },
+        body: JSON.stringify({ authUser: token.id }),
         cache: "no-store",
-      }
+      },
     );
 
     if (!upstream.ok) {
       const text: string = await upstream.text();
       return NextResponse.json(
         { error: text || "Failed to fetch notifications" },
-        { status: upstream.status }
+        { status: upstream.status },
       );
     }
 
